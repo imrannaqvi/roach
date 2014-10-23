2aadca0335b42c531b039c4d7054e76369a9e0e9
Imran Naqvi <imrannaqvi83@yahoo.com>
2014-10-23 21:39:14 +0500

    #3 - some re-factoring.

--- zend/module/API/src/API/Controller/IndexController.php
+++ zend/module/API/src/API/Controller/IndexController.php
@@ -8,10 +8,8 @@ class IndexController extends AbstractActionController
 {
 	public function indexAction()
 	{
-		$out = array(
-			'error' => false,
-			'response' => false
-		);
+		$error = false;
+		$response = false;
 		//get config
 		$config = $this->getServiceLocator()->get('Config');
 		if(! array_key_exists('roach', (array) $config)) {
@@ -36,35 +34,26 @@ class IndexController extends AbstractActionController
 					//check if model method is specified
 					if(array_key_exists('method', $item)) {
 						if(method_exists($model, $item['method'])) {
-							$out['response'] = call_user_func_array(array($model, $item['method']), array());
+							$response = call_user_func_array(array($model, $item['method']), array());
 						} else {
-							$out = array_merge($out, array(
-								'error' => 'model-method-not-found'
-							));
+							$error = 'model-method-not-found';
 						}
 					} else {
-						$out = array_merge($out, array(
-							'error' => 'model-method-not-defined'
-						));
+						$error = 'model-method-not-defined';
 					}
 				} catch( \Zend\ServiceManager\Exception\ServiceNotFoundException $e) {
-					$out = array_merge($out, array(
-						'error' => 'model-not-found-as-service'
-					));
+						$error = 'model-not-found-as-service';
 				}
 			} else {
-				$out = array_merge($out, array(
-					'error' => 'model-not-defined'
-				));
+				$error = 'model-not-defined';
 			}
 		} else {
-			$out = array_merge($out, array(
-				'error' => 'method-not-found'
-			));
+			$error = 'method-not-found';
 		}
-		return new JsonModel(array_merge($out, array(
-			'method' => $method,
-			//'config' => $config
-		)));
+		return new JsonModel(array(
+			'error' => $error,
+			'response' => $response,
+			'method' => $method
+		));
 	}
 }
