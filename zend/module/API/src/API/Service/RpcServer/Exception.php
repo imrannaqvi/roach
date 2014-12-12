e46622024d96f9aab52b19c3986aff90ce0ecc04
Imran Naqvi <imrannaqvi83@yahoo.com>
2014-12-12 23:43:00 +0500

    #6 - added phpdoc.

--- zend/module/API/src/API/Service/RpcServer/Exception.php
+++ zend/module/API/src/API/Service/RpcServer/Exception.php
@@ -3,8 +3,19 @@ namespace API\Service\RpcServer;
 
 class Exception extends \Exception
 {
+	/** @var array Used for passing data. */
 	private $_options;
 	
+	/**
+	 * Constructor
+	 *
+	 * @param string $message
+	 * @param int $code
+	 * @param Exception|null $previous
+	 * @param array $options
+	 *
+	 * @return boolean
+	 */
 	public function __construct(
 		$message,
 		$code = 0,
@@ -15,6 +26,12 @@ class Exception extends \Exception
 		$this->_options = $options;
 	}
 	
+	/**
+	 * Getter for options.
+	 * @param array $options
+	 *
+	 * @return array
+	 */
 	public function getOptions()
 	{
 		return $this->_options;
