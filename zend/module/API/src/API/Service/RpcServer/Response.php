c12782413abf2846372b9d4f85a5672b86d871f5
Imran Naqvi <imrannaqvi83@yahoo.com>
2014-12-15 22:31:47 +0500

    #6 - added phpdoc block for API\Service\RpcServer\Response.

--- zend/module/API/src/API/Service/RpcServer/Response.php
+++ zend/module/API/src/API/Service/RpcServer/Response.php
@@ -3,17 +3,36 @@ namespace API\Service\RpcServer;
 
 class Response
 {
+	/** @var null|array */
 	public $exception = null;
+	
+	/** @var mixed */
 	public $error = false;
+	
+	/** @var null|array */
 	public $response = null;
+	
+	/** @var string */
 	public $method;
 	
+	/**
+	 * Set Error for current response object.
+	 *
+	 * @param mixed.
+	 *
+	 * @return API\Service\RpcServer\Response
+	 */
 	public function setError($error)
 	{
 		$this->error = $error;
 		return $this;
 	}
 	
+	/**
+	 * Return response as array.
+	 *
+	 * @return array
+	 */
 	public function toArray()
 	{
 		return array(
