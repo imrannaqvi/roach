d32a911b4c84e68337dd0901aa0cfaaab040f7b8
Imran Naqvi <imrannaqvi83@yahoo.com>
2014-12-24 23:43:21 +0500

    #9 - login request.

--- public/app/authCtrl.js
+++ public/app/authCtrl.js
@@ -1,3 +1,8 @@
 app.controller('authCtrl', function ($scope, $rootScope, $routeParams, $location, $http, API) {
-	
+	//initially set those objects to null to avoid undefined error
+	$scope.login = function (user) {
+		API.login(user, function(data){
+			console.log(data);
+		});
+	};
 });
