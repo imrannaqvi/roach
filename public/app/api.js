b646a3ac93a63d5384e80a102a1df28e5eee6c8d
Imran Naqvi <imrannaqvi83@yahoo.com>
2014-12-24 23:42:36 +0500

    #10 - base request method and login method.

--- public/app/api.js
+++ public/app/api.js
@@ -1,4 +1,25 @@
 app.factory("API", ['$http', function ($http) {
-	var serviceBase = 'api/';
-	return {};
+	var serviceBaseUrl = 'api/';
+	return {
+		login: function(user, callback) {
+			this.request('login', user, callback);
+		},
+		request: function(method, params, callback){
+			$http({
+				method: 'POST',
+				url: serviceBaseUrl,
+				data: {
+					method: method,
+					params: params
+				}
+			}).success(function(data) {
+				console.info('API[' + method + ']', data);
+				if(! data.error && typeof(callback) === typeof(function(){})) {
+					callback(data.response);
+				}
+			}).error(function(data, status, headers, config) {
+				console.error('API[' + method + ']', data, status, headers, config);
+			});
+		}
+	};
 }]);
