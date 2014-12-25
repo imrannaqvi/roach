b1d0ade0152decafc9fc22035e91a5913d2a33df
Imran Naqvi <imrannaqvi83@yahoo.com>
2014-12-26 02:14:20 +0500

    #9 - changes due to deferred/promises in API and error incorrect login.

--- public/app/authCtrl.js
+++ public/app/authCtrl.js
@@ -1,8 +1,14 @@
 app.controller('authCtrl', function ($scope, $rootScope, $routeParams, $location, $http, API) {
 	//initially set those objects to null to avoid undefined error
-	$scope.login = function (user) {
-		API.login(user, function(data){
-			console.log(data);
+	$scope.login_error = false;
+	$scope.user = {};
+	$scope.login = function () {
+		API.login($scope.user).then(function(data){
+			$scope.login_error = false;
+			console.log('login success:', data);
+		}, function(data){
+			$scope.login_error = true;
+			console.log('login failure:', data);
 		});
 	};
 });
