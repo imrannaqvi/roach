10c83a44fa0adadac1791ab5e86cc5f0ced8db0b
Imran Naqvi <imrannaqvi83@yahoo.com>
2014-12-31 23:31:49 +0500

    #9 - logout functionality.

--- public/app/app.js
+++ public/app/app.js
@@ -9,6 +9,8 @@ app.config(['$routeProvider', 'localStorageServiceProvider', function ($routePro
 		title: 'Login',
 		templateUrl: 'partials/login.html',
 		controller: 'AuthenticationController'
+	}).when('/logout', {
+		title: 'Logout'
 	}).when('/signup', {
 		title: 'Signup',
 		templateUrl: 'partials/signup.html',
@@ -22,19 +24,29 @@ app.config(['$routeProvider', 'localStorageServiceProvider', function ($routePro
 		templateUrl: 'partials/404.html'
 	});
 }])
-.run(function ($rootScope, $location, API) {
+.run(function ($rootScope, $location, API, localStorageService) {
 	$rootScope.logout = function(){
 		console.log('$rootScope.logout():');
 	};
 	//TODO: request session by using token from localStorage on other than login url
 	$rootScope.$on("$routeChangeStart", function (event, next, current) {
+		console.log('$routeChangeStart:', event, next, current);
+		//next.$$route.originalPath
+		if(next.$$route.originalPath === '/logout') {
+			$rootScope.user = false;
+			$rootScope.acl = false;
+			API.token = false;
+			API.user = false;
+			localStorageService.remove(API.token_key);
+			$location.path("/login");
+			return;
+		}
 		//send session request
 		if(
 			! $rootScope.user && 
 			( ! next.$$route || 
 			['/login', '/signup'].indexOf(next.$$route.originalPath) === -1 )
 		) {
-			console.log('request session::', $rootScope.user);
 			API.getSession().then(function(data){
 				console.log('got session::', $rootScope.user, data);
 			}, function(reason){
