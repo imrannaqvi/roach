4778e20b55d1e89e1cede2e131fd50cdae386fd3
Imran Naqvi <imrannaqvi83@yahoo.com>
2015-01-19 19:23:41 +0500

    #7 - changed controller to favorite object notation.

--- public/app/controllers/authentication.js
+++ public/app/controllers/authentication.js
@@ -2,15 +2,17 @@
 
 app.controller('AuthenticationController', function ($scope, $rootScope, $routeParams, $location, $http, API) {
 	//initially set those objects to null to avoid undefined error
-	$scope.login_error = false;
-	$scope.login = function () {
-		API.login($scope.authentication).then(function(data){
-			$scope.login_error = false;
-			console.log('login success:', data);
-			$location.path('/');
-		}, function(data){
-			$scope.login_error = true;
-			console.log('login failure:', data);
-		});
-	};
+	angular.extend($scope, {
+		login_error: false,
+		login: function () {
+			API.login($scope.authentication).then(function(data){
+				$scope.login_error = false;
+				console.log('login success:', data);
+				$location.path('/');
+			}, function(data){
+				$scope.login_error = true;
+				console.log('login failure:', data);
+			});
+		}
+	});
 });
