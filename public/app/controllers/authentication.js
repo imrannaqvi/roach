1185db30e460115dbd8f7ba79182ced5f62a8ecf
Imran Naqvi <imrannaqvi83@yahoo.com>
2015-01-26 22:34:51 +0500

    #18 - updated form and controller.

--- public/app/controllers/authentication.js
+++ public/app/controllers/authentication.js
@@ -4,7 +4,7 @@ app.controller('AuthenticationController', function ($scope, $rootScope, $routeP
 	//initially set those objects to null to avoid undefined error
 	angular.extend($scope, {
 		login_error: false,
-		login: function () {
+		login: function(){
 			API.login($scope.authentication).then(function(data){
 				$scope.login_error = false;
 				console.log('login success:', data);
@@ -12,6 +12,14 @@ app.controller('AuthenticationController', function ($scope, $rootScope, $routeP
 			}, function(data){
 				$scope.login_error = true;
 				console.log('login failure:', data);
+			});
+		},
+		signUp: function(){
+			console.log(this.signup);
+			API.request('signup', this.signup, true).then(function(data){
+				console.log('signup success:', data);
+			}, function(data){
+				console.log('signup failure:', data);
 			});
 		}
 	});
